28bcf0c289b765cb4d3427e68264f64cc6a2943c
Michael Manke <michaelmanke00@gmail.com>
2022-08-09 15:02:40 +0200

    Built shopping List

--- ShoppingList/src/helper/listFunctions.ts
+++
@@ -1,8 +0,0 @@
-import { createItemOrder, createLocalStore } from '../store/localStore';
-
-export const [store, setStore] = createLocalStore([
-  { title: 'Recipies', items: [{ title: '' }] },
-  { title: 'Shopping List', items: [] },
-]);
-
-export const [itemOrder, setItemOrder] = createItemOrder([]);
